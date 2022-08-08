3b7c6095db3db9086e8269a6c6afa0afdcce3cae
Mateen <mateen937@gmail.com>
2022-08-08 21:28:09 +0530

    2n commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import {Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Nav from './components/Nav';
 import Services from './components/Services';
+import PageNotFount from './components/PageNotFount';
 
 function App() {
   return (
-    <> 
-   
- {/*    <Routes >
-      <Route path='/' element={ <Home/> } />
-      <Route path='/about' element={ <About />} />
-    </Routes> */}
-  
+    <>
 
-  <Nav/>
-  <Routes>
-      <Route path='/' element={<Home/>}></Route> 
-      <Route path='/about' element={<About/>}></Route>
-      <Route path='/servies' element= {<Services /> }  />
-  </Routes>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/servies' element={<Services />} />
+
+      </Routes>
     </>
-  
+
   );
 }
 
